a2a7a55d93b0a6fab946ab447c8a7f37d9c21879
bhanu <bpst.web@gmail.com>
2022-05-01 18:26:20 +0530

    session code

--- application/home/routes.py
+++ application/home/routes.py
@@ -1,7 +1,8 @@
 # import required packages
 from application.home import blueprint
+from flask import render_template
 
 
-@blueprint.route('/')
-def index():
-    return 'welcome to actual home'
+@blueprint.route('/dashboard')
+def dashboard():
+    return render_template('home/index.html', title='Flask Templates', username='bhanu')
--- application/image/routes.py
+++ application/image/routes.py
@@ -1,7 +1,8 @@
 # import packages
 from application.image import blueprint
+from flask import render_template
 
 
 @blueprint.route('/image-classification')
 def image_classification():
-    return 'image classification'
+    return render_template('image/image-classification.html', title='Flask Templates', bhanu='image classification 1')
